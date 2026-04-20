ae514a103d02a594b09bcc6e8a9194f3caf6daed
aebonlee <aebon@kyonggi.ac.kr>
2026-04-21 00:06:56 +0900

    chore: 최고관리자 이메일 3인 통일 (admin config)

--- src/config/admin.ts
+++ src/config/admin.ts
@@ -1,1 +1,1 @@
-export const ADMIN_EMAILS = ['aebonlee@gmail.com', 'aebon@kakao.com', 'aebon@kyonggi.ac.kr'];
+export const ADMIN_EMAILS = ['aebon@kakao.com', 'radical8566@gmail.com', 'aebon@kyonggi.ac.kr'];
